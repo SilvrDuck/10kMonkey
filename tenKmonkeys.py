4f65e34aa7b91e93bf37d471960f8d24f211d0ed
Thibault <thibault.viglino@epfl.ch>
2019-10-04 10:25:22 +0200

    fix

--- tenKmonkeys.py
+++ tenKmonkeys.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from gpt_2 import model, sample, encoder
+from gpt_2.src import model, sample, encoder
 
 
 def interact_model(
@@ -17,27 +17,24 @@ def interact_model(
     temperature=1,
     top_k=0,
     top_p=1,
-    models_dir='gpt-2/models',
+    models_dir='../gpt_2/models',
 ): 
      
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
-    if batch_size is None:
-        batch_size = 1
-        assert nsamples % batch_size == 0
 
-        enc = encoder.get_encoder(model_name, models_dir)
-        hparams = model.default_hparams()
-        with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
-            hparams.override_from_dict(json.load(f))
+    enc = encoder.get_encoder(model_name, models_dir)
+    hparams = model.default_hparams()
+    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
 
     with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
+        context = tf.placeholder(tf.int32, [1, None])
         np.random.seed(seed)
         tf.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
-            batch_size=batch_size,
+            batch_size=1,
             temperature=temperature, top_k=top_k, top_p=top_p
         )
 
